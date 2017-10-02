065951bed41f58610d5df7607f5f0ac2bbb021a2
thuytiensp <thuytiensp@gmail.com>
2017-10-02 11:55:54 +0700

    Route group

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 Route::get('/', function () {
     return view('blog.index');
 })->name('blog.index');
-
 Route::get('post/{id}', function () {
     return view('blog.post');
 })->name('blog.post');
@@ -23,20 +22,24 @@ Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
 
-Route::get('admin/create', function () {
-    return view('admin.create');
-})->name('admin.create');
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+    Route::post('create', function() {
+        return "It works!";
+    })->name('admin.create');
 
-Route::post('admin/create', function () {
-    return "It works!";
-})->name('admin.create');
-
-Route::get('admin', function () {
-    return view('admin.index');
-})->name('admin.index');
-Route::get('admin/edit/{id}', function () {
-    return view('admin.edit');
-})->name('admin.edit');
-
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+    
+    Route::post('edit', function() {
+        return "It works!";
+    })->name('admin.update');
+});
 
