47bb56412b345be01207c0a446476bc263966716
thuytiensp <thuytiensp@gmail.com>
2017-10-02 10:45:16 +0700

    Using partial, Bootstrap, NavBar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,18 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+			
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+			
         <title>Hello Laravel</title>
     </head>
     <body>
-        @yield('content')
+    		
+    		@include('partials.navigation')
+
+    		<div class="container">
+    			@yield('content')
+    		</div>       	
+
     </body>
 </html>
