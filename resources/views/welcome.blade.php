994fd94ac5c369a08043b48b267fe67a97324371
thuytiensp <thuytiensp@gmail.com>
2017-10-02 10:49:18 +0700

    Control Struture

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,19 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-12">
-        <h1>Hello Laravel</h1>
+    <div class="col-md-12">        
+        <h1>Control Struture</h1>
+            @if(true)
+                <p>This is only display if if is true</p>
+            @else
+                <p>This is only display if if is false</p>
+            @endif
+
+            <hr>
+            
+            @for($i = 0; $i < 5; $i++)
+                <p>{{$i + 1}} Iteration</p>
+            @endfor
     </div>
 </div>
 
