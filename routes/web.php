250d7d3ce7374222d4d6e905e1fa726cec5e0bb8
thuytiensp <thuytiensp@gmail.com>
2017-10-02 11:26:01 +0700

    Add some routes and Views for admin, post and other

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,24 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('blog.index');
+})->name('blog.index');
+
+Route::get('post/{id}', function () {
+    return view('blog.post');
+})->name('blog.post');
+
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+
+
+Route::get('admin/create', function () {
+    return view('admin.create');
+})->name('admin.create');
+Route::get('admin', function () {
+    return view('admin.index');
+})->name('admin.index');
+Route::get('admin/edit/{id}', function () {
+    return view('admin.edit');
+})->name('admin.edit');
