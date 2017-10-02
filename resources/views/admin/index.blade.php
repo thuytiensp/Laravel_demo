9affec8001c4543b0fc09b43eef238a24a3929cd
thuytiensp <thuytiensp@gmail.com>
2017-10-02 15:39:18 +0700

    Add some methods for Post Model, Post Controller, validation in PostController, Refactor Route

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,7 +20,11 @@
 
 <div class="row">
 	<div class="col-md-12">
-		<p><strong>Learning Laravel</strong><a href="{{ route('admin.edit', ['id' => 1]) }}"> Edit</a></p>
+
+		@foreach($posts as $post)
+		<p><strong>{{ $post['title']}}</strong><a href="{{ route('admin.edit', ['id' => array_search($post, $posts)]) }}"> Edit</a></p>
+		@endforeach
+
 	</div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,82 +11,40 @@
 |
 */
 
-Route::get('/', 'PostController@getIndex')->name('blog.index');
+Route::get('/', [
+    'uses' => 'PostController@getIndex',
+    'as' => 'blog.index'
+]);
 
-Route::get('post/{id}', function ($id) {
-   if($id == 1)
-	{
-		$post = [
-				'title' => 'Learning Laravel',
-				'content' => 'This blog post will get you right on track with Laravel'
-		];
-	}else{
-		$post = [
-						'title' => 'Something else',
-						'content' => 'Some other content'
-				];
-	}
-    return view('blog.post', ['post' => $post]);
-})->name('blog.post');
-
+Route::get('post/{id}', [
+    'uses' => 'PostController@getPost',
+    'as' => 'blog.post'
+]);
 
 Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('', function () {
-        return view('admin.index');
-    })->name('admin.index');
-
-    Route::get('create', function () {
-        return view('admin.create');
-    })->name('admin.create');
-
-    Route::post('create', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
-    	
-    	$validation = $validator->make($request->all(),[
-    		'title' => 'required|min:5',
-    		'content' => 'required|min:10'
-    	]);
-    	
-    	if($validation->fails())
-    		return redirect()->back()->withErrors($validation);
-
-        return redirect()
-        ->route('admin.index')
-        ->with('info','Post created, Title: ' . $request->input('title'));
-    })->name('admin.create');
-
-    Route::get('edit/{id}', function ($id) {
-    	if($id == 1)
-		{
-			$post = [
-					'title' => 'Learning Laravel',
-					'content' => 'This blog post will get you right on track with Laravel'
-			];
-		}else{
-			$post = [
-							'title' => 'Something else',
-							'content' => 'Some other content'
-					];
-		}
-      return view('admin.edit', ['post' => $post]);
-    })->name('admin.edit');
-
-    Route::post('edit', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
-
-    	$validation = $validator->make($request->all(),[
-    		'title' => 'required|min:5',
-    		'content' => 'required|min:10'
-    	]);
-
-    	if($validation->fails())
-    		return redirect()->back()->withErrors($validation);
-
-         return redirect()
-        			->route('admin.index')
-        			->with('info','Post editted, new Title: ' . $request->input('title'));
-    })->name('admin.update');
+    Route::get('', [
+        'uses' => 'PostController@getAdminIndex',
+        'as' => 'admin.index'
+    ]);
+    Route::get('create', [
+        'uses' => 'PostController@getAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::Post('create', [
+        'uses' => 'PostController@postAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::Get('edit/{id}', [
+        'uses' => 'PostController@getAdminEdit',
+        'as' => 'admin.edit'
+    ]);
+    
+    Route::Post('edit', [
+        'uses' => 'PostController@postAdminUpdate',
+        'as' => 'admin.update'
+    ]);
 });
-
